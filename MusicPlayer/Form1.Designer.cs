39e8a5d225be97a0fecdaf7e0e39647e0ffc1adf
MHRZz17 <admmqtt190743@gmail.com>
2024-02-26 18:46:29 +0330

    First desine .

--- MusicPlayer/Form1.Designer.cs
+++ MusicPlayer/Form1.Designer.cs
@@ -29,15 +29,219 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            progressBar1 = new ProgressBar();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            label4 = new Label();
+            button7 = new Button();
+            panel1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(128, 128, 255);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button6);
             panel1.Location = new Point(-1, 0);
             panel1.Name = "panel1";
             panel1.Size = new Size(1258, 48);
             panel1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 71);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(319, 681);
+            tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(311, 648);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(242, 92);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(panel4);
+            panel2.Controls.Add(panel3);
+            panel2.Location = new Point(345, 95);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(900, 657);
+            panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button3);
+            panel3.Controls.Add(button2);
+            panel3.Controls.Add(button1);
+            panel3.Controls.Add(progressBar1);
+            panel3.Location = new Point(16, 512);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(867, 131);
+            panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(label3);
+            panel4.Location = new Point(16, 15);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(222, 482);
+            panel4.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(513, 402);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(513, 453);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(86, 20);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(253, 35);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(587, 335);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(248, 62);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(595, 29);
+            progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(161, 62);
+            button1.Name = "button1";
+            button1.Size = new Size(52, 29);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(86, 62);
+            button2.Name = "button2";
+            button2.Size = new Size(52, 29);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(17, 62);
+            button3.Name = "button3";
+            button3.Size = new Size(52, 29);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(129, 12);
+            button4.Name = "button4";
+            button4.Size = new Size(52, 29);
+            button4.TabIndex = 4;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(71, 12);
+            button5.Name = "button5";
+            button5.Size = new Size(52, 29);
+            button5.TabIndex = 5;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(13, 12);
+            button6.Name = "button6";
+            button6.Size = new Size(52, 29);
+            button6.TabIndex = 6;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(1179, 16);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 20);
+            label4.TabIndex = 5;
+            label4.Text = "label4";
+            // 
+            // button7
+            // 
+            button7.Location = new Point(1193, 60);
+            button7.Name = "button7";
+            button7.Size = new Size(52, 29);
+            button7.TabIndex = 7;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -46,16 +250,48 @@
             BackColor = Color.FromArgb(192, 192, 255);
             ClientSize = new Size(1257, 785);
             ControlBox = false;
+            Controls.Add(button7);
+            Controls.Add(panel2);
+            Controls.Add(tabControl1);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             Text = "Form1";
             WindowState = FormWindowState.Maximized;
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label1;
+        private Panel panel4;
+        private Label label3;
+        private Panel panel3;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private ProgressBar progressBar1;
+        private Label label4;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
     }
 }
